07165412fa33c043619447a086aba1e6d2678657
Annabel Väljaots <114983045+annabelv@users.noreply.github.com>
2023-02-13 14:43:52 +0200

    Delete teisendus.ts

--- 01_funktsioon/teisendus.ts
+++
@@ -1,8 +0,0 @@
-//teisendame tunnid minutiteks
-
-let klaviatuur=require("readline-sync")
-let tunnid:number = klaviatuur.question("Palun sisesta tunnid: ")
-console.log("Sisestatud tunnid minutites on "+tunnid*60);
-
-/*Palun sisesta tunnid: 7
-Sisestatud tunnid minutites on 420*/
