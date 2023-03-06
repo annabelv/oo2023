61c41cdaf223a7ee150586b7381ddaefc49209c5
Annabel Väljaots <114983045+annabelv@users.noreply.github.com>
2023-03-06 15:53:42 +0200

    Add files via upload

---
+++ 05_kontrolltoo1/KT_1.ts
@@ -0,0 +1,7 @@
+/* Koosta funktsioon, mis korrutab parameetritena antud kaks arvu ning võtab neist ruutjuure (Math.sqrt). Katseta.*/
+
+function geom_keskmine(arv1:number, arv2:number) {
+	return (Math.sqrt(arv1 * arv2))
+}
+
+console.log (geom_keskmine(2, 8))
---
+++ 05_kontrolltoo1/KT_2.ts
@@ -0,0 +1,13 @@
+/* Arve võib olla rohkem, need antakse ette kogumina. Arvud korrutatakse kokku ning võetakse niimitmes juur (astendaja pöördväärtus, Math.pow), kuimitu arvu oli.*/
+
+let arvud:Array<number> = [1, 4, 7];
+let a = 1;
+
+function korrutis (arvud:Array<number>):number {	
+	for (let i = 0; i < arvud.length; ++i) { 
+	  a *= arvud[i]; 
+	} 
+	return (Math.pow(a, arvud.length));
+}
+
+console.log (korrutis(arvud))
