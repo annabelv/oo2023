f2932594401cadd01c6f524578aa57c8c5886ebe
Annabel Väljaots <114983045+annabelv@users.noreply.github.com>
2023-03-06 15:59:01 +0200

    Delete KT_1.ts

--- 05_kontrolltoo1/KT_1.ts
+++
@@ -1,7 +0,0 @@
-/* Koosta funktsioon, mis korrutab parameetritena antud kaks arvu ning võtab neist ruutjuure (Math.sqrt). Katseta.*/
-
-function geom_keskmine(arv1:number, arv2:number) {
-	return (Math.sqrt(arv1 * arv2))
-}
-
-console.log (geom_keskmine(2, 8))
